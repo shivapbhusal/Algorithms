487e04aaeec1f927b534ffb8a86612f01a70ed55
shivapbhusal <civabhusal@gmail.com>
2017-10-15 13:19:19 -0400

    insertion and selection sort added

---
+++ sorting/insertion.py
@@ -0,0 +1,24 @@
+# Python implementation of selection sort 
+
+def insertion(num_list, size): # Selection sort function
+    for i in range(1, size):
+        key=num_list[i]
+        j=i-1
+        while (j>0) and (num_list[j]>key):
+            num_list[j+1]=num_list[j]
+            j=j-1
+        num_list[j+1]=key
+
+fin=open('data.txt') # Open the file
+num_list=[]
+
+for lines in fin:    # Read numbers from the file and create an unsorted list 
+    lines=int(lines)
+    num_list.append(lines)
+
+n=len(num_list)
+insertion(num_list,n)
+print(num_list)
+
+
+
