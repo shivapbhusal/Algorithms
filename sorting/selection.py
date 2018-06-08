4b1a7f0c7bf2f720aa8f9d65ff6d028946d34f82
shivapbhusal <civabhusal@gmail.com>
2018-06-07 20:49:37 -0400

    refactor selection.py

--- sorting/selection.py
+++ sorting/selection.py
@@ -1,10 +1,18 @@
 '''
-Python implementation of selection sort
-Complexity: N2 
+Python implementation of selection sort.
+Takes the fileName with numbers as the argument.
+
+Running the program:
+python selection.py $filename
+Complexity: O(N^2) 
 
 '''
 
-def selection(num_list, size): # Selection sort function
+import sys
+
+
+def selection(num_list): # Selection sort function
+    size=len(num_list)
     for i in range(0,size-1):
         for j in range(i,size-1):
             if num_list[j]<num_list[i]: # Swap if num_list[j] is less than num_list[i]
@@ -12,15 +20,14 @@ def selection(num_list, size): # Selection sort function
                 num_list[i]=num_list[j]
                 num_list[j]=temp
 
-fin=open('data.txt') # Open the file
+fin=open(sys.argv[1]) # Open the file
 num_list=[]
 
 for lines in fin:    # Read numbers from the file and create an unsorted list 
     lines=int(lines)
     num_list.append(lines)
 
-n=len(num_list)
-selection(num_list,n)
+selection(num_list)
 print(num_list)
 
 
