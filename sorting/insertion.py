9d921a5d675a5408c3ee2cc43303f1c04f67b7d4
shivapbhusal <civabhusal@gmail.com>
2018-06-07 22:41:38 -0400

    add insertion.py

--- sorting/insertion.py
+++ sorting/insertion.py
@@ -1,23 +1,33 @@
-# Python implementation of selection sort 
+'''
+Python implementation of Insertion sort. 
+Takes a data file as an argument.
+Example, python insertion.py data.txt as a input. 
+'''
 
-def insertion(num_list, size): # Selection sort function
+import sys
+
+def insertion(num_list):
+    '''
+    Insertion sort function. 
+    Argument type: list
+    '''
+    size =len(num_list)
     for i in range(1, size):
         key=num_list[i]
         j=i-1
-        while (j>0) and (num_list[j]>key):
+        while (j>=0) and (num_list[j]>key):
             num_list[j+1]=num_list[j]
             j=j-1
         num_list[j+1]=key
 
-fin=open('data.txt') # Open the file
+fin=open(sys.argv[1]) # Open the file
 num_list=[]
 
 for lines in fin:    # Read numbers from the file and create an unsorted list 
     lines=int(lines)
     num_list.append(lines)
 
-n=len(num_list)
-insertion(num_list,n)
+insertion(num_list)
 print(num_list)
 
 
